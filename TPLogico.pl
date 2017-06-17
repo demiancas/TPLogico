4959bea67ca5bd26df1c3346be7581542e091684
NicolasGuidoBarbeito <nicolasguidobarbeito@gmail.com>
2017-06-17 15:33:31 -0300

    Modifico Base de Conocimiento y Predicado esFiel
    Agrego datos en la base de conocimientos: dos parejas nuevas, trabajos de bernardo y george y agrego predicado esFiel que indica que una persona es fiel cuando sale con una única persona. Ademas, borro los comentarios en % que son irrelevantes para el código.

--- TPLogico.pl
+++ TPLogico.pl
@@ -1,14 +1,24 @@
-%pareja(Persona, Persona)
 pareja(marsellus, mia).
 pareja(pumkin,    honeyBunny).
- 
-%trabajaPara(Empleador, Empleado)
+pareja(bernardo, bianca).
+pareja(bernardo, charo).
+
 trabajaPara(marsellus, vincent).
 trabajaPara(marsellus, jules).
 trabajaPara(marsellus, winston).
+trabajaPara(vincent, bernardo).
+trabajaPara(winston, bernardo).
+trabajaPara(bianca, george).
+trabajaPara(charo, george).
 
 saleCon(Persona1, Persona2):-
 	pareja(Persona1, Persona2).
 
 saleCon(Persona1, Persona2):-
 	pareja(Persona2, Persona1).
+
+esFiel(Persona):-
+	saleCon(Persona, _),
+	findall(Persona,saleCon(Persona,_),Parejas),
+	length(Parejas,CantidadDeParejas),
+	CantidadDeParejas = 1.
