aed6b46809e8c888889ad88e79458ced139e330f
demiancas <demiancas@hotmail.com>
2017-06-17 14:56:54 -0300

    Agrego predicado, "saleCon"
    1er punto del TP

--- TPLogico.pl
+++ TPLogico.pl
@@ -6,3 +6,9 @@ pareja(pumkin,    honeyBunny).
 trabajaPara(marsellus, vincent).
 trabajaPara(marsellus, jules).
 trabajaPara(marsellus, winston).
+
+saleCon(Persona1, Persona2):-
+	pareja(Persona1, Persona2).
+
+saleCon(Persona1, Persona2):-
+	pareja(Persona2, Persona1).
